5cf724c633d69fbd9749b8c4ef8b53d38a0eaec9
AnastasiaKoto <104349916+AnastasiaKoto@users.noreply.github.com>
2022-04-26 14:29:53 +0500

    Add files via upload

---
+++ check_phone.js
@@ -0,0 +1,59 @@
+function check ()
+{
+  var files = DriveApp.getFolderById('1kid83EFbvMgSIJ5vOOHXdRSqvhaXK19O').getFiles();
+  var idGroup = [];
+  var dateObj = new Date();
+  var nameSubstring = dateObj.getDate();
+  nameSubstring += '.';
+  nameSubstring += dateObj.getMonth() + 1;
+  
+  while (files.hasNext()) {
+  var file = files.next();
+   if (file.getName().substring(0, 4) == nameSubstring) {
+    idGroup.push(file.getId());
+    }
+  }
+
+  if ((idGroup.length != 0)) {
+    idGroup.forEach(function(sheets){
+    var doc = SpreadsheetApp.openById(sheets).getSheetByName("итоговый график");
+    
+      if (doc) {
+        this.searchErrors(fileId);
+      }
+    })
+  }
+}
+
+function searchErrors(fileId) {
+  var ss = SpreadsheetApp.openById(fileId);
+  var phone = [], phone2 = [], cell = [];  
+  ss.getSheets().forEach(function(fileId) {
+    if (fileId.getSheetName() == "итоговый график") {
+      for (let i = 1; i <= fileId.getLastRow(); i++) {
+        cell = ['E' + i, fileId.getRange('E' + i).getValue()] ;
+        phone.push(cell);
+      }
+    }
+    if (fileId.getSheetName() == "Лист 1") {
+      for (let i = 1; i <= fileId.getLastRow(); i++) {
+        phone2.push(fileId.getRange('G' + i).getValue());
+      }
+    }
+    phone.forEach(function(number) {
+      
+      if(!phone2.includes(number[1])) {
+        ss.getSheetByName('итоговый график').getRange(number[0]).setBackground('#f5424e');
+      }
+    });
+  });
+}
+
+function onOpen(doc) 
+{
+  var ui = doc.get;
+  console.log(ui);
+  doc.createMenu('Меню')
+  .addItem('Выполнить', 'searchErrors')
+  .addToUi();
+}
